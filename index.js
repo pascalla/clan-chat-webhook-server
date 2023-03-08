04d60838e75c51c1e71a1a45a2b827e77966c0dd
Joshua Blackman <joshua.blackman@Joshuas-Mac-Mini.local>
2023-03-08 13:23:31 +0000

    update iron

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ fastify.post('/webhook', async (request, reply) => {
             messageToSend = `**${message.author}**: ${message.content}`
 
             messageToSend = messageToSend.replace('<img=0>', '<:Player_moderator_emblem:1082981033340833804>')
-            messageToSend = messageToSend.replace('<img=9>', '<:Ironman_chat_badge:1082980848200065034>')
+            messageToSend = messageToSend.replace('<img=2>', '<:Ironman_chat_badge:1082980848200065034>')
             messageToSend = messageToSend.replace('<img=10>', '<:Hardcore_ironman_chat_badge:1082980846887243826> ')
             messageToSend = messageToSend.replace('<img=11>', '<:Ultimate_ironman_chat_badge:1082980849571602532>')
         }
